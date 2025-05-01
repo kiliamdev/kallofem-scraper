4f8c4f2ec0934a8c738112474e2e6c952fc02c96
kiliamdev <kiliam.dev0@gmail.com>
2025-05-01 12:30:36 +0200

    Fix: Flask port binding for Render

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from kallofem_scraper.spiders.products_spider import ProductsSpider
+import os
 
 app = Flask(__name__)
 
@@ -15,3 +16,7 @@ def run_scraper():
     process.crawl(ProductsSpider)
     process.start()
     return jsonify({"message": "Scraper lefutott, az output.json fájl frissült."})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
