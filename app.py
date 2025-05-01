bddaf687ff805a9d591267720b68624516a56da9
kiliamdev <kiliam.dev0@gmail.com>
2025-05-01 16:09:57 +0200

    Fix: updated app.py (changes for main and scrape page)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, send_file, render_template
+from flask import Flask, jsonify, send_file, render_template, redirect
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from kallofem_scraper.spiders.products_spider import ProductsSpider
@@ -11,7 +11,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     has_output = os.path.exists("output.json")
-    return render_template("index.html", has_output=has_output)
+    log_output = None
+    if os.path.exists("scraper_log.txt"):
+        with open("scraper_log.txt", "r", encoding="utf-8") as f:
+            log_output = f.read()[-3000:]  # csak az utolsó 3000 karakter
+    return render_template("index.html", has_output=has_output, log=log_output)
 
 @app.route("/scrape", methods=["POST"])
 def run_scraper():
@@ -20,23 +24,16 @@ def run_scraper():
     if os.path.exists("output.json"):
         os.remove("output.json")
 
-    result = subprocess.run(["scrapy", "crawl", "products"], capture_output=True, text=True)
+    with open("scraper_log.txt", "w", encoding="utf-8") as log_file:
+        result = subprocess.run(["scrapy", "crawl", "products"], stdout=log_file, stderr=subprocess.STDOUT, text=True)
     
     for _ in range(10):
         if os.path.exists("output.json"):
             break
         time.sleep(0.5)
 
-    log_output = result.stdout + "\n\n" + result.stderr
-    has_output = os.path.exists("output.json")
-
-    from datetime import datetime
-    
-    generate_time = None
-    if has_output:
-        generate_time = datetime.now().strftime("%Y.%m.%D %H:%M:%S")
-
-    return render_template("index.html", log=log_output[-3000:], has_output=has_output)
+    has_output = os.path.exists("output.json") 
+    return redirect("/")
 
     
 @app.route("/output")
