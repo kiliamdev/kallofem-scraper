d65895e4de34ab23262868fc3231ae5e0658b1f1
kiliamdev <kiliam.dev0@gmail.com>
2025-05-01 17:17:41 +0200

    Fix: output preview issue fixed, restartable scraper, redirect replaced

--- app.py
+++ app.py
@@ -11,51 +11,38 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     has_output = os.path.exists("output.json")
-    output_preview = None
+    output_preview = []
 
     if has_output:
         try:
             import json
             with open("output.json", "r", encoding="utf-8") as f:
                 data = json.load(f)
-                output_preview = data[:5]
+                if isinstance(data,list):
+                    output_preview = data[:5]
         except Exception as e:
-            output_preview = [{"hiba": str(e)}]
+            output_preview = [{"hiba": f"hiba történt a JSON olvasása során:{str(e)}"}]
 
     return render_template("index.html", has_output=has_output, output_preview=output_preview)
 
 @app.route("/scrape", methods=["POST"])
 def run_scraper():
-    import time
     import run_spider
-    import sys
-    from io import StringIO
+    import os
+    import time
 
     if os.path.exists("output.json"):
         os.remove("output.json")
 
-    old_stdout = sys.stdout
-    mystdout = StringIO()
-    sys.stdout = mystdout
+    run_spider.run()
 
-    try:
-        run_spider.run()
-    except Exception as e:
-        print(f"\nHiba: {e}\n")
-
-    sys.stdout = old_stdout
-
-    log_output = mystdout.getvalue()
-    with open("scraper_log.txt", "w", encoding="utf-8") as f:
-        f.write(log_output())
-    
+    # várunk picit, hogy biztos létrejöjjön a fájl
     for _ in range(10):
         if os.path.exists("output.json"):
             break
-        time.sleep(0.5)
+        time.sleep(0.3)
 
     return redirect("/")
-
     
 @app.route("/output")
 def get_output():
--- run_spider.py
+++ run_spider.py
@@ -1,8 +1,19 @@
-from scrapy.crawler import CrawlerProcess
+from twisted.internet import reactor
+from scrapy.crawler import CrawlerRunner
 from scrapy.utils.project import get_project_settings
 from kallofem_scraper.spiders.products_spider import ProductsSpider
+from twisted.internet.task import react
+import sys
+
+def run_spider_main():
+    runner = CrawlerRunner(get_project_settings())
+
+    deferred = runner.crawl(ProductsSpider)
+    deferred.addBoth(lambda _: reactor.stop())
+    reactor.run()  # blocking call
 
 def run():
-    process = CrawlerProcess(get_project_settings())
-    process.crawl(ProductsSpider)
-    process.start()
+    try:
+        run_spider_main()
+    except RuntimeError:
+        pass
