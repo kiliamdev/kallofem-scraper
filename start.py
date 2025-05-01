3d5d49ba901a65ef1a75538fdf94d19139ccd014
kiliamdev <kiliam.dev0@gmail.com>
2025-05-01 12:28:09 +0200

    removed start.py

--- start.py
+++
@@ -1,7 +0,0 @@
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-from kallofem_scraper.spiders.products_spider import ProductsSpider
-
-process = CrawlerProcess(get_project_settings())
-process.crawl(ProductsSpider)
-process.start()
